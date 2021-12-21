e2f4c3d54f90b03cd9d700b2d6a82b43399ec71d
Bell <belliy@ad.vzwcorp.com>
2021-12-21 17:05:37 -0500

    styled warehouse forms

--- public/index.js
+++ public/index.js
@@ -1,9 +1,11 @@
-const light = document.getElementById("home");
+const light = document.getElementById("lightItUp");
+const dark = document.getElementById("home");
 let change = true;
 
-light.addEventListener('click', function modeChange(){
+dark.addEventListener('click', function modeChange(){
     if(change){
         this.style.backgroundImage = "url('https://images.unsplash.com/photo-1590209680469-534b21078b1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80')";
+        light.style.backgroundImage = "url('https://images.unsplash.com/reserve/oGLumRxPRmemKujIVuEG_LongExposure_i84.jpeg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1773&q=80')";
         change = false;
     } else {
         this.style.backgroundImage = "url('https://images.unsplash.com/reserve/oGLumRxPRmemKujIVuEG_LongExposure_i84.jpeg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1773&q=80')";
